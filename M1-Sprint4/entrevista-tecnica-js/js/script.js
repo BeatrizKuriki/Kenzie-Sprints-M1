bb634b68b77e7ce1d26b3e5fd10fd98a07ec5502
Beatriz Alencar Kuriki <b.f.kuriki@gmail.com>
2022-12-12 13:40:07 -0300

    entrevista

---
+++ M1-Sprint4/entrevista-tecnica-js/js/script.js
@@ -0,0 +1,9 @@
+let idade = Number(prompt('Qual sua idade?'))
+if(idade>=60){
+    alert('Você está autorizado a comprar o ingresso com 50% de desconto!')
+}else if(idade>= 18){
+    alert('Você está autorizado a comprar!');
+
+}else{
+    alert('Você não está autorizado a comprar o ingresso!');
+}
