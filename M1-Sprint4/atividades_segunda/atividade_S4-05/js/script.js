3e9bb3df986d5126709c3357b2a1003ddb88b43e
Beatriz Alencar Kuriki <b.f.kuriki@gmail.com>
2022-12-12 15:08:56 -0300

    b=nova maneira de imprimir a sequencia de fibonacci

--- M1-Sprint4/atividades_segunda/atividade_S4-05/js/script.js
+++ M1-Sprint4/atividades_segunda/atividade_S4-05/js/script.js
@@ -1,21 +1,22 @@
 function fibonacciNumbers(x){
     let anterior =0;
-    let atual =1;
-    let proximo=0;
-    let sequencia = '0, ';
-    for(let i = 0; i <= x; i++){ 
-        proximo= atual + anterior;
-        sequencia = sequencia  + proximo + ', ';
-        anterior = atual;
-        atual = proximo;      
-      
+    let atual = 1;
+    let proximo =0;
+    
+    for(let i = 0; i < x; i++){ 
+        console.log(proximo);
+        anterior = atual + proximo;
+        atual = proximo;
+        proximo = anterior;
+        
        
     }
-    return sequencia;
+    
    
 }
 console.log(fibonacciNumbers(15));
 
 //========================================================================
 //EXERCÍCIO 2
-console.log('===============EXERCÍCIO 2==================================')
+console.log('===============EXERCÍCIO 2==================================');
+
