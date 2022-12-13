b111551f957a8ad1d47c4169f2a6a1b2bb48e1a2
Beatriz Alencar Kuriki <b.f.kuriki@gmail.com>
2022-12-13 11:14:25 -0300

    exercício 1 e ex3 resolvidos

--- M1-Sprint4/atividades_terça/atividade_S4-07/js/script.js
+++ M1-Sprint4/atividades_terça/atividade_S4-07/js/script.js
@@ -1,12 +1,13 @@
 //EXERCÍCIO 1
 
-//let valor = Number(prompt('Digite um valor entre 0 e 10:'));
-//while(valor >= 0 || valor < 10){    
-   // alert('Código correto!');
-    //valor++;
-//}
-//alert('Valor inválido!');
- //valor= Number(prompt('Digite novamente!'));
+let valor = Number(prompt('Digite um valor entre 0 e 10:'));
+while(valor < 0 || valor > 10){ 
+    alert('Valor inválido!');
+    valor = Number(prompt('Digite um valor entre 0 e 10:'));  
+}
+alert('Código correto!');
+
+
 
  //=======================================================================
  //EXERCÍCIO 2
@@ -19,4 +20,23 @@
     nome=prompt('Digite o nome do usuário:');
     senha = prompt('Digite a senha:');
  }
- alert('Usuário e senha corretos, acessando o sistema...')
+ alert('Usuário e senha corretos, acessando o sistema...');
+
+ //=======================================================================
+ //EXERCÍCIO 3
+ alert('exercício 3');
+
+ let num = parseInt(prompt('Digite um número positivo e menor que 10'));
+ let soma = 0;
+ let quantidade =0;
+ let media =0;
+ while(num < 10 && num > 0){
+    soma = soma + num;
+    num = parseInt(prompt('Digite um número positivo e menor que 10'));
+    quantidade++;
+ }
+ media = soma/quantidade;
+
+ alert(`A soma dos números inteiros e positivos é igual a: ${soma} e a méida é igual a ${media} `)
+
+
