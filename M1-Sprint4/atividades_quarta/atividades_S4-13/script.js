6a82ca7c58de3cebc75bbccab1eda9601f547e23
Beatriz Alencar Kuriki <b.f.kuriki@gmail.com>
2022-12-18 15:39:35 -0300

    consertar exercicio 5

---
+++ M1-Sprint4/atividades_quarta/atividades_S4-13/script.js
@@ -0,0 +1,86 @@
+//EXERCÍCIO 1
+console.log('=====================EXERCÍCIO 1============================');
+function geraArray (palavra){
+    let palavraNova = palavra.split('');
+  
+    
+    return palavraNova;
+}
+   
+
+
+console.log(geraArray('leite'));
+
+//EXERCÍCIO 2
+console.log('=====================EXERCÍCIO 2============================');
+let arr = ['Hermanoteu', 'Olonéia', 'Migalatéia', 'Godart'];
+
+function acessaElemento(number) {
+    let resultado;
+    if( number > arr.length){
+        return 'valor não encontrado';
+
+    }  
+    for (let i = 0; i <= arr.length; i++) {
+        if (number == i) {
+            resultado = arr[i];
+            return resultado;
+        }        
+       
+    }
+    
+}      
+
+console.log(acessaElemento(1));
+
+
+
+//EXERCÍCIO 3
+console.log('=====================EXERCÍCIO 3============================');
+let arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+function testeParImpar(numero) {
+
+    for (let i = 0; i <= arr2.length; i++) {
+        console.log(arr2[numero]);
+        //console.log(arr2[i]);
+        if(arr2[numero] % 2 == 0){
+            return 'Valor par'
+        }else{
+            return 'Valor impar'
+        }      
+        
+
+    }    
+
+}
+
+console.log(testeParImpar(3));
+
+
+//EXERCÍCIO 4
+console.log('=====================EXERCÍCIO 4============================');
+
+let nomes1 =['Pedro', 'Rafael', 'José'];
+
+function quantidadeCaractere(arr){
+    let maior = 0;
+    for(let i =0; i<= arr.length-1; i++){
+       console.log(arr[i])
+       if(arr[0].length > 0){
+        maior = arr[0];
+       } else if(arr[1].length > arr[0].length){
+        maior = arr[i]
+
+       }
+
+        }
+        console.log("-----"+maior+"-------")
+
+    } 
+
+
+quantidadeCaractere(nomes1);
+
+//EXERCÍCIO 5
+console.log('=====================EXERCÍCIO 5============================');
+
