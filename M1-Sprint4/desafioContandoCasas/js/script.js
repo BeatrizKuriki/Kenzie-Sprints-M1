65789f663e27dd0e4b91490c958abb6c7de7e2d5
Beatriz Alencar Kuriki <b.f.kuriki@gmail.com>
2022-12-24 10:40:16 -0300

    função numeroAleatorio finalizada

---
+++ M1-Sprint4/desafioContandoCasas/js/script.js
@@ -0,0 +1,11 @@
+
+function gerarNumeroAleatorio(){
+    const numeroAleatorio = Math.round(Math.random() * 20);
+    return numeroAleatorio;
+}  
+gerarNumeroAleatorio();
+
+function verificarPalpite(){
+    let tentativa = 1;
+
+}
