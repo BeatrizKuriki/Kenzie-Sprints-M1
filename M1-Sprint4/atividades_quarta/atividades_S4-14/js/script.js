80553319ba77f8b8b62dbb91d5c976504c73b791
Beatriz Alencar Kuriki <b.f.kuriki@gmail.com>
2022-12-25 09:07:06 -0300

    exercícios 1,2 e 3

---
+++ M1-Sprint4/atividades_quarta/atividades_S4-14/js/script.js
@@ -0,0 +1,53 @@
+const listaDeElementos = [1,5,7,9,3,'10','11','15'];
+
+function retornaTodosElementos(){
+    let retornaLista = [];
+    for(let i = 0; i<= listaDeElementos.length -1; i++){
+        if(i== (listaDeElementos.length -1)){
+            retornaLista += listaDeElementos[i]
+
+        }else{
+            retornaLista += listaDeElementos[i]+", ";
+        }        
+    }
+    return '['+retornaLista+']';
+}
+
+console.log((retornaTodosElementos()));
+
+console.log('===========================EXERCÍCIO 2============================================');
+//EXERCÍCIO 2
+function retornaValorEspecífico(num){
+    let valor;
+    for(let i =0; i<= listaDeElementos.length -1; i++){
+        if(num == i){
+            valor= listaDeElementos[i];
+        }
+    }
+    return `O valor contido nesta posição é: ${valor}`;
+
+}
+console.log(retornaValorEspecífico(5));
+
+console.log('===========================EXERCÍCIO 3============================================');
+//EXERCÍCIO 3
+function retornarTipoElemento(n){
+    let tipo;
+    for(let i = 0; i <= listaDeElementos.length-1; i++){
+        if(n == i){
+            tipo +=listaDeElementos[i];
+        }
+
+    }
+    return ` O elemento ${n} é um elemento do tipo:`+typeof tipo;
+
+}
+console.log(retornarTipoElemento(0));
+
+
+
+
+
+
+console.log('===========================EXERCÍCIO 4============================================');
+//EXERCÍCIO 4
