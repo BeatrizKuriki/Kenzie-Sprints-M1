76ec72ae7dee86c58d113aae73cecfa03905c66d
Beatriz Alencar Kuriki <b.f.kuriki@gmail.com>
2022-12-27 12:25:17 -0300

    exercício 2

--- M1-Sprint4/atividade_quinta_codeTracing/atividades_S4-17/js/script.js
+++ M1-Sprint4/atividade_quinta_codeTracing/atividades_S4-17/js/script.js
@@ -19,3 +19,34 @@ console.log(retornaParImpar(arr));
 
 //EXERCÍCIO 2
 console.log('===============================EXERCÍCIO 2==========================');
+let arrA = Array(10);
+
+function percentualImpar(arrA){
+    let impar = 0;
+    let percentual =0;
+    for(let i =0; i <= arrA.length; i++){
+        if(i % 2 != 0){
+            impar++
+        }
+        
+    }
+    percentual = (impar*100)/10;
+    return `O total de valores impares é ${impar} e corresponde a ${percentual.toFixed(2)}% da lista`;
+}
+
+console.log(percentualImpar(arrA));
+
+//EXERCÍCIO 3
+console.log('===============================EXERCÍCIO 3==========================');
+let arrayA = Array(10);
+let arrayB = Array(10);
+
+function retornaListaC(arrayA, arrayB){    
+    for(let i =0; i<= 10; i++){
+        if(%2==0 && i%3==0){
+            console.log(arrayA += i);
+
+        }
+    }
+}
+retornaListaC(arrayA, arrayB)
